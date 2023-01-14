41177a1e658c46ac2148dfbcf07f316051790f06
Alexandra <118150358+kssaxa@users.noreply.github.com>
2023-01-14 21:27:21 +1000

    Add files via upload

---
+++ Ассемблер лаба 1 номер 2.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    short A, B, C;
+    _asm
+    {
+        mov ax, 00001010b //10
+        mov bx, 00011110b //30
+        mov cx, 00101111b //47
+        mov dx, 11110000b// маска дл умножение bx и ax
+        shl cx,4  //cx=1111000
+        or ax, cx   //лог сложение страшей части первого с младшей частью третьего//ax=11111010
+        or bx,dx // разделение младшей bx = 11111110
+        and ax,bx //лог умножение младших частей первого и второго чисел// ax=11111010(250)
+
+       shr ax,2 //деление на 4 //ax=62dec
+   
+        mov A, ax
+
+
+        
+    }
+    cout << A;
+
+}
---
+++ Ассемблер лабораторная 2.cpp
@@ -0,0 +1,55 @@
+﻿
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	short y, y1, y2, x, a,Y1,Y2;
+	
+	std::cout << "a is "; std::cin >>a;
+	std::cout << "x is "; std::cin >> x;
+	_asm
+	{
+		mov cx, 1;
+		mov ax, a
+		mov bx, x
+		cmp ax, bx
+		je Ravn
+
+			Neravn:
+		mov cx, ax
+			sub cx, bx
+			jmp label_1;
+		Ravn:
+		mov cx, bx
+			add cx, 3
+
+			label_1 :
+			mov y1, cx;
+
+
+
+
+			label_3:
+	cmp ax,bx
+			jl Men
+
+			mov cx,ax
+			sub cx,bx
+			jmp label_2
+		
+			Men :
+		mov cx,ax
+			label_2:
+		mov y2,cx
+
+
+
+
+	}
+	y = y1 * y2;
+	printf("%d\n", y1);
+	printf("%d\n", y2);
+	printf("%d\n", y);
+	return 0;
+}
